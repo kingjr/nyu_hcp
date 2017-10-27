c84b08fcd2becf8ad803d73ce86da1ab91b84d9b
kingjr <jeanremi.king@gmail.com>
2017-10-27 18:54:10 -0400

    fix

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -6,7 +6,7 @@ c = get_config()
 c.IPKernelApp.pylab = 'inline'  # if you want plotting support always in your notebook
 
 # Notebook config
-c.NotebookApp.certfile = u'/home/ubuntu/certs/mycert.pem' #location of your certificate file
+# c.NotebookApp.certfile = os.path.expanduser('~/certs/mycert.pem') #location of your certificate file
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False  #so that the ipython notebook does not opens up a browser by default
 # It is a good idea to put it on a known, fixed port
